c1c0e044a042aaba5edfc253db4a6484b1a63778
Vasu Dasari <vdasari@gmail.com>
2020-12-11 22:16:17 -0500

    ofp-monitor: Extend Flow Monitoring support for OF 1.0+ with Nicira Extensions
    Currently OVS supports flow-monitoring for OpenFlow 1.0 and Nicira Extenstions.
    Any other OpenFlow versioned messages are not accepted. This checkin will allow
    OpenFlow1.0+ Flow Monitoring wth Nicira extensions be accepted. Also made sure
    that flow-monitoring updates, flow monitoring pause messages, resume messages
    are sent in the same OpenFlow version as that of flow-monitor request.
    
    Description of changes:
    
    1. Generate ofp-msgs.inc to be able to support 1.0+ Flow Monitoring messages.
    include/openvswitch/ofp-msgs.h
    
    2. Support vconn to accept user specified version and use it for vconn flow-monitoring session
    ofproto/ofproto.c
    
    3. Modify APIs to use protocol as an argument to encode and decode messages
    include/openvswitch/ofp-monitor.h
    include/openvswitch/vlog.h
    lib/ofp-monitor.c
    ofproto/connmgr.c
    ofproto/connmgr.h
    ofproto/ofproto.c
    
     4. Testcase for verification
    tests/ofproto.at
    
    Signed-off-by: Vasu Dasari <vdasari@gmail.com>
    Reported-at: https://mail.openvswitch.org/pipermail/ovs-discuss/2020-December/050820.html
    Signed-off-by: Vasu Dasari <vdasari@gmail.com>

--- ofproto/ofproto.c
+++ ofproto/ofproto.c
@@ -6351,7 +6351,8 @@ static void
 ofproto_compose_flow_refresh_update(const struct rule *rule,
                                     enum nx_flow_monitor_flags flags,
                                     struct ovs_list *msgs,
-                                    const struct tun_table *tun_table)
+                                    const struct tun_table *tun_table,
+                                    enum ofputil_protocol protocol)
     OVS_REQUIRES(ofproto_mutex)
 {
     const struct rule_actions *actions;
@@ -6374,14 +6375,15 @@ ofproto_compose_flow_refresh_update(const struct rule *rule,
     fu.ofpacts_len = actions ? actions->ofpacts_len : 0;
 
     if (ovs_list_is_empty(msgs)) {
-        ofputil_start_flow_update(msgs);
+        ofputil_start_flow_update(msgs, protocol);
     }
     ofputil_append_flow_update(&fu, msgs, tun_table);
 }
 
 void
 ofmonitor_compose_refresh_updates(struct rule_collection *rules,
-                                  struct ovs_list *msgs)
+                                  struct ovs_list *msgs,
+                                  enum ofputil_protocol protocol)
     OVS_REQUIRES(ofproto_mutex)
 {
     struct rule *rule;
@@ -6391,7 +6393,7 @@ ofmonitor_compose_refresh_updates(struct rule_collection *rules,
         rule->monitor_flags = 0;
 
         ofproto_compose_flow_refresh_update(rule, flags, msgs,
-                ofproto_get_tun_tab(rule->ofproto));
+                ofproto_get_tun_tab(rule->ofproto), protocol);
     }
 }
 
@@ -6555,7 +6557,8 @@ handle_flow_monitor_request(struct ofconn *ofconn, const struct ovs_list *msgs)
 
     struct ovs_list replies;
     ofpmp_init(&replies, ofpbuf_from_list(ovs_list_back(msgs))->header);
-    ofmonitor_compose_refresh_updates(&rules, &replies);
+    ofmonitor_compose_refresh_updates(&rules, &replies,
+                                      ofconn_get_protocol(ofconn));
     ovs_mutex_unlock(&ofproto_mutex);
 
     rule_collection_destroy(&rules);
